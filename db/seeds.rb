470e487a1203bdfd0dbd54cec346ea8d686747e5
danielmoi <daniel@ampersandmoi.com>
2016-04-12 18:20:41 +1000

    add Flight seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Flight.destroy_all
+flight1 = Flight.create({ flight_number: 'BA432', origin: 'Sydney', destination: 'London', departure_date: '2016-04-11'});
+flight2 = Flight.create({ flight_number: 'BA122', origin: 'New York', destination: 'San Francisco', departure_date: '2016-04-10'});
+flight3 = Flight.create({ flight_number: 'BA765', origin: 'Lisbon', destination: 'Tokyo', departure_date: '2016-04-18'});
