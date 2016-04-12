f81e710027767b07240760027c5329bc36e3a3e3
danielmoi <daniel@ampersandmoi.com>
2016-04-12 20:39:42 +1000

    add airplane seed data and add airplane_id to Flight model

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,6 +8,7 @@
 #  password_digest :text
 #  created_at      :datetime         not null
 #  updated_at      :datetime         not null
+#  admin           :boolean          default("false")
 #
 
 class User < ActiveRecord::Base
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160412062147) do
+ActiveRecord::Schema.define(version: 20160412103610) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -29,8 +29,9 @@ ActiveRecord::Schema.define(version: 20160412062147) do
     t.text     "origin"
     t.text     "destination"
     t.date     "departure_date"
-    t.datetime "created_at",     null: false
-    t.datetime "updated_at",     null: false
+    t.datetime "created_at",                 null: false
+    t.datetime "updated_at",                 null: false
+    t.integer  "airplane_id",    default: 0
   end
 
   create_table "reservations", force: :cascade do |t|
