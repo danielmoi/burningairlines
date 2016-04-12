fc63273b6f7f9969fcf6c43b1a3256e9722cab76
danielmoi <daniel@ampersandmoi.com>
2016-04-13 08:34:46 +1000

    access seatID upon clicking seat div

--- app/assets/javascripts/views/FlightReservationView.js
+++ app/assets/javascripts/views/FlightReservationView.js
@@ -4,17 +4,28 @@ app.FlightReservationView = Backbone.View.extend({
 
   el: '#flight-seats',
 
+  events: {
+    'click .seats__col': 'displaySeatID'
+  },
+
+  displaySeatID: function(event) {
+    console.log(event.currentTarget.dataset.seatId);
+  },
+
   render: function(flight) {
     this.$el.empty();
     $('#flight-seats__heading').text('Seats');
     console.log('flightReservationView initiated');
-    var rows = this.model.attributes.airplane.rows;
+    var arrLetters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    var rowNumbers = this.model.attributes.airplane.rows;
     var cols = this.model.attributes.airplane.columns;
 
-    _.times(rows, function(row) {
-      var $row = $('<div>', { class: 'seats__row', 'data-seat-id': row + 1 });
+    _.times(rowNumbers, function(rowNumber) {
+      var rowLetter = arrLetters[rowNumber];
+
+      var $row = $('<div>', { class: 'seats__row' });
       _.times(cols, function(col) {
-        var $col = $('<div>', { class: 'seats__col', 'data-seat-id': col + 1 });
+        var $col = $('<div>', { class: 'seats__col', 'data-seat-id': rowLetter + (col + 1) });
         $row.append( $col );
       });
       $('#flight-seats').append( $row );
