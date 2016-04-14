6a9dcd1640b400ab271ffbbc533f3b433ad9fc4a
danielmoi <daniel@ampersandmoi.com>
2016-04-14 11:27:56 +1000

    implement search functionality using jquery

--- app/assets/javascripts/views/AppView.js
+++ app/assets/javascripts/views/AppView.js
@@ -3,6 +3,32 @@ var app = app || {};
 app.AppView = Backbone.View.extend({
   el: '#main',
 
+  events: {
+    'click #button-search': 'magicSearch',
+    'click #button-all': 'magicAll'
+  },
+
+  magicSearch: function() {
+    var flightOrigin = $('#search-origin').val().trim();
+    var flightDestination = $('#search-destination').val().trim();
+
+    if (flightOrigin !== '') {
+      console.log('searching');
+      $('.flight-details__origin').filter(function(index, element) {
+        return $(element).html() !== flightOrigin;
+      }).parent().hide();
+    }
+    if (flightDestination !== '') {
+      $('.flight-details__destination').filter(function(index, element) {
+        return $(element).html() !== flightDestination;
+      }).parent().hide();
+    }
+  },
+  magicAll: function() {
+    $('#flights').children().show();
+  },
+
+
   render: function() {
     console.log('appView initiated');
     var appViewTemplate = $('#appViewTemplate').html();
